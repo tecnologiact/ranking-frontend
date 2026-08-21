0b129e4cfe799ad5753ad05b68cae28931b43a62
tecnologiact <tecnologia@ciadetalentos.com>
2026-08-21 15:22:06 -0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const nextConfig = {
     return [
       {
         source: "/api/proxy/:path*",
-        destination: "https://beckend-ranking.vercel.app/:path*",
+        destination:
+          (process.env.NEXT_PUBLIC_API_URL ||
+            "https://backend-ranking-backend-ranking.p7agvn.easypanel.host") +
+          "/:path*",
       },
     ];
   },
